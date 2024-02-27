9f44a2796471985be5a4f79ed8665af9e368a387
Candice Thomason <candice.thomason@gmail.com>
2024-02-27 20:09:27 +1100

    first 3 tests

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!';
+} else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+} else if (distanceInFeet > 2500) {
+    return 'No can do.';
+}
 }
 
 function ternaryCheckCity(){
